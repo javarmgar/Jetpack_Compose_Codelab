248966f1649ba37c26f9db33b7160f40e207defb
Javier Armenta <j.armentagarcia@fetchrewards.com>
2023-10-17 16:06:39 -0600

    arrangment basics

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -22,7 +22,9 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
@@ -30,6 +32,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -241,9 +245,9 @@ Scales:
 */
 @Composable
 fun AlignYourBodyElement(
-    modifier: Modifier = Modifier,
     @DrawableRes drawable: Int,
-    @StringRes text: Int
+    @StringRes text: Int,
+    modifier: Modifier = Modifier,
 ) {
 
     Column(
@@ -311,11 +315,47 @@ fun FavoriteCollectionCard(
 }
 
 // Step: Align your body row - Arrangements
+/*
+Alignment: cross-axis
+    For a Column, the cross-axis is the horizontal axis.
+    For a Row, the cross-axis is the vertical axis.
+
+Arrangment: main-axis
+    How to place child composable on a container's main axis
+    for Row: horizontal
+    for Column: vertical
+
+Link
+https://developer.android.com/codelabs/jetpack-compose-layouts?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-layouts#6
+
+    - equal-weight
+    - space between
+    - space around
+    - space evenly
+    - start  - top
+    - center - center
+    - end    - bottom
+
+Arrangement.spacedBy() method to add a fixed space in between each child composable.
+
+
+ */
+
 @Composable
 fun AlignYourBodyRow(
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
+
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ){
+        items(alignYourBodyData) { item ->
+            AlignYourBodyElement(item.drawable, item.text)
+        }
+    }
+
 }
 
 // Step: Favorite collections grid - LazyGrid
@@ -411,9 +451,9 @@ fun SearchBarPreview() {
 fun AlignYourBodyElementPreview() {
     MySootheTheme {
         AlignYourBodyElement(
-            modifier = Modifier.padding(8.dp),
             drawable = R.drawable.ab1_inversions,
             text = R.string.ab1_inversions,
+            modifier = Modifier.padding(8.dp),
         )
     }
 }
